e95699d9c704493631ef31414e8f617b6fee1f99
afflatusAnimus <FabularAegis@gmail.com>
2025-06-04 16:55:40 +0300

    lesson 2 step 8 - new verifications for test_guest_can_go_to_login_page() added

--- test_main_page.py
+++ test_main_page.py
@@ -5,6 +5,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
