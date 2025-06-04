bfb6a6a5a2ac2246502eb6825d97a1eb0f635313
afflatusAnimus <FabularAegis@gmail.com>
2025-06-04 16:26:22 +0300

    test modified according to Page Object Model

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+from stepik_selenium_chapter4_project.conftest import browser
+
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
