0b225d095a332b6ac8aca5ec6f5a9fb2487ee24a
afflatusAnimus <FabularAegis@gmail.com>
2025-06-05 09:23:44 +0300

    login page redirect

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,19 +1,10 @@
 from .base_page import BasePage
-from .locators import LoginPageLocators
 from .locators import MainPageLocators
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
-    def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
-
-    def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
-
-    def should_be_login_url(self):
-        assert "login" in self.browser.current_url, "Login URL doesn't contain substring 'login'"
