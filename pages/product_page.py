19a51075cd861d98574477e84e2ee1ee0a9b501d
afflatusAnimus <FabularAegis@gmail.com>
2025-06-05 10:30:45 +0300

    param test, 1 test with xfail

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,7 +8,11 @@ class ProductPage(BasePage):
         link.click()
 
     def should_be_correct_cart_price(self):
-        assert self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text == self.browser.find_element(*ProductPageLocators.CART_PRICE_ALERT).text, "Diff betwenn cart price and price of added product"
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        cart_price_alert = self.browser.find_element(*ProductPageLocators.CART_PRICE_ALERT).text
+        assert book_price == cart_price_alert, f"Diff between cart price '{cart_price_alert}' and price of added product '{book_price}'"
 
     def should_be_correct_product_name(self):
-        assert self.browser.find_element(*ProductPageLocators.BOOK_NAME).text == self.browser.find_element(*ProductPageLocators.BOOK_ADDED_TO_CART_NAME).text, "Diff between book name and book added to cart name"
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        book_added_to_cart_name =  self.browser.find_element(*ProductPageLocators.BOOK_ADDED_TO_CART_NAME).text
+        assert book_name == book_added_to_cart_name, f"Diff between book name '{book_name}' and book added to cart name '{book_added_to_cart_name}'"
